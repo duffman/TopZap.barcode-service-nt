1b7979c80617876076dff0e388b0be024e10edb5
duffman <patrik.forsberg@coldmind.com>
2019-02-24 16:58:16 +0100

    m

---
+++ topzap.barcode-service-nt/dist/service_2.js
@@ -0,0 +1,1 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});const logger_1=require('./app/cli/logger');const app_1=require('./app/app');let serviceName='Music Magpie';let serviceKey='mmp';logger_1.Logger.logPurple('Starting Service ::',serviceName);let app=new app_1.Application(true,serviceKey);
---
+++ topzap.barcode-service-nt/dist/service_3.js
@@ -0,0 +1,1 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});const logger_1=require('./app/cli/logger');const app_1=require('./app/app');let serviceName='Ziffit';let serviceKey='ziffit';logger_1.Logger.logPurple('Starting Service ::',serviceName);let app=new app_1.Application(true,serviceKey);
